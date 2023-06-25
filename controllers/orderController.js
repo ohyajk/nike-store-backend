7183e080ee6651bd1b4934f6967c534515e2eac4
ohyajk <ohyajk@gmail.com>
2023-06-25 19:52:16 +0530

    Remove cart api

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,45 +1,23 @@
 import Order from '../models/orderModel.js';
-import Cart from '../models/cartModel.js';
 
 // Post an order
 export const addOrder = async (req, res) => {
     try {
-        const { user, shippingAddress } = req.body;
+        const { user, items, subTotal, tax, grandTotal, shippingAddress } = req.body;
 
-        // Find the user's cart and populate the items
-        const cart = await Cart.find({ user }).populate('product');
-        console.log(cart)
-        if (!cart) {
-            return res.status(404).json({ error: 'Cart not found' });
-        }
-
-        const items = cart.map((item) => ({
-            product: item.product,
-            size: item.size, // Include the size property
-            quantity: item.quantity,
-            price: item.product.price,
-        }));
-
-        // Calculate the total amount
-        const totalAmount = items.reduce((total, item) => {
-            return total + item.price * item.quantity;
-        }, 0);
-
-        // Create a new order
         const order = new Order({
             user,
             items,
-            totalAmount,
+            subTotal,
+            tax,
+            grandTotal,
             shippingAddress,
+            status: 'pending',
         });
 
-        // Save the order to the database
         await order.save();
-
-        // Delete the user's cart
-        await Cart.deleteMany({ user });
-
         res.status(201).json(order);
+
     } catch (err) {
         res.status(500).json({ message: 'An error occurred while creating the order', err });
     }
--- server.js
+++ server.js
@@ -5,11 +5,12 @@ import dotenv from 'dotenv'
 import connectDB from './db.js'
 import userRoute from './routes/userRoute.js'
 import productRoute from './routes/productRoute.js'
-import cartRoute from './routes/cartRoute.js'
 import orderRoute from './routes/orderRoute.js'
+
 const app = express()
 dotenv.config()
 const api = process.env.API_PATH
+
 // Middleware
 app.use(express.json())
 app.use(cors())
@@ -21,8 +22,8 @@ connectDB()
 // USE ROUTES
 app.use(api, userRoute)
 app.use(api, productRoute)
-app.use(api, cartRoute)
 app.use(api, orderRoute)
+
 // PORT
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`App listening at port http://localhost:${PORT}/api`))
