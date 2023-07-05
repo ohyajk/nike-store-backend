e5524f9665c292364703679d46796edcddb4a7e7
ohyajk <ohyajk@gmail.com>
2023-07-05 08:54:46 +0530

    add credentials triue

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ const api = process.env.API_PATH
 
 // Middleware
 app.use(express.json())
-app.use(cors())
 app.use(cookieParser());
+app.use("*", cors({
+    origin: true,
+    credentials: true
+}))
 
 // Connect DB
 connectDB()
