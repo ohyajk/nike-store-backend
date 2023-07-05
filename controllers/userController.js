533fecf535ba505be87a7a67450f0317e9c7f62a
ohyajk <ohyajk@gmail.com>
2023-07-05 09:58:34 +0530

    change to res send

--- controllers/userController.js
+++ controllers/userController.js
@@ -96,10 +96,21 @@ export const login = async (req, res) => {
         const token = jwt.sign({ user: user._id }, process.env.SECRET, { expiresIn: '1h' });
 
         // Set cookies as strings
-        const tokenCookie = `token=${encodeURIComponent(token)}; Secure; HttpOnly; SameSite=None; Expires=${new Date(Date.now() + 3600000).toUTCString()}`;
-        const idCookie = `id=${encodeURIComponent(userId)}; Secure; SameSite=None; Expires=${new Date(Date.now() + 3600000).toUTCString()}`;
+        const tokenCookie = encodeURIComponent(token);
+        const idCookie = encodeURIComponent(userId);
 
-        res.setHeader('Set-Cookie', [tokenCookie, idCookie]);
+        res.cookie('token', tokenCookie, {
+            secure: true, // Set to true if using HTTPS
+            httpOnly: true,
+            sameSite: 'none',
+            expires: new Date(Date.now() + 3600000), // Expires in 1 hour
+        });
+        res.cookie('id', idCookie, {
+            secure: true, // Set to true if using HTTPS
+            httpOnly: true,
+            sameSite: 'none',
+            expires: new Date(Date.now() + 3600000), // Expires in 1 hour
+        });
         res.status(200).json({ msg: 'Logged In Sire ...' });
     } catch (error) {
         res.status(500).json({ error: 'Invalid Credentials or Server Error...' });
