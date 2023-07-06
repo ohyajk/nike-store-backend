35f62c98bf5e1e18c75694efe314bd5455892fea
ohyajk <ohyajk@gmail.com>
2023-07-06 08:44:34 +0530

    console.log req cookies

--- auth.js
+++ auth.js
@@ -1,13 +1,12 @@
 import jwt from 'jsonwebtoken';
 export const auth = (req, res, next) => {
-    const cookies = req.cookie;
-    console.log(cookies)
-    if (!cookies?.token) {
+    console.log(req.cookies)
+    if (!req.cookies) {
         return res.status(401).json({ message: 'Unauthorized: Missing token' });
     }
 
-    const { token } = cookies;
-
+    const { token } = req.cookies;
+    console.log(token)
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: 'Unauthorized: Invalid token' });
