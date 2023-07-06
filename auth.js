c22f7eb5e24e6d70c07be11b1b03d4c2c9d33d20
ohyajk <ohyajk@gmail.com>
2023-07-06 21:32:57 +0530

    use authorization header

--- auth.js
+++ auth.js
@@ -1,11 +1,10 @@
 import jwt from 'jsonwebtoken';
 export const auth = (req, res, next) => {
-    console.log(req.cookies)
-    if (!req.cookies) {
+    const token = req.headers['authorization'];
+    if (!token) {
         return res.status(401).json({ message: 'Unauthorized: Missing token' });
     }
 
-    const { token } = req.cookies;
     console.log(token)
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
         if (err) {
